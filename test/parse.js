14b5f2697772cf875bdc75384a50f5cacc08f202
koorchik <koorchik@gmail.com>
2014-07-02 20:23:27 +0300

    Start working on validator

--- test/parse.js
+++ test/parse.js
@@ -1,10 +1,21 @@
 'use strict';
 
 var SAMLParser = require('./../lib/SAMLParser');
+var SAMLValidator = require('./../lib/SAMLValidator');
+
 var fs = require('fs');
+var util = require('util');
 
 // console.log(__dirname + '/fixtures/valid_saml_token.xml');
 var validTokenStr = fs.readFileSync( __dirname + '/fixtures/valid_saml_token.xml' ).toString();
 
-var parser = new SAMLParser({namespace: 'ns2'});
-parser.parse(validTokenStr);
+var parser = new SAMLParser({ namespace: 'ns2' });
+var validator = new SAMLValidator();
+
+parser.parse(validTokenStr).then(function(assertion) {
+    // console.log(util.inspect(assertion, { depth: 100 }));
+    return validator.validate(assertion);
+}).then(function(validAssertion) {
+    // console.log(util.inspect(validAssertion, {depth: 100}));
+}).catch(console.error);
+
